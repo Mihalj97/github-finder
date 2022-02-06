cada689de76c5a6e432808b26b5ac0cd13eec83b
Mihalj97 <mihalj.tandi5@gmail.com>
2022-02-06 18:46:09 +0100

    Clear Users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -35,6 +35,8 @@ export const GithubProvider = ({ children }) => {
               payload: items,
         })
     }
+    // Clear users from state
+    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
 
     // Set loading
     const setLoading = () => dispatch({type:'SET_LOADING'})
@@ -42,7 +44,8 @@ export const GithubProvider = ({ children }) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        searchUsers
+        searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
